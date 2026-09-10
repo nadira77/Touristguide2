5eb97a04c01fe3be82716106fa39298825acb9d3
nadir77 <nahi1000@stud.ek.dk>
2026-09-10 15:42:29 +0200

    alle

---
+++ src/test/java/com/example/touristguide2/TouristGuide2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.touristguide2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristGuide2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
