2465d84f4446ae14c7a6381fa4a721f08fb3d17e
celineewu <celineewu@gmail.com>
2026-09-11 11:14:45 +0200

    add TouristAttraction from ver.1

--- src/main/java/Model/TouristAttraction.java
+++ src/main/java/Model/TouristAttraction.java
@@ -1,4 +1,19 @@
 package Model;
 
 public class TouristAttraction {
+    private final String name;
+    private final String description;
+
+    public TouristAttraction(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
