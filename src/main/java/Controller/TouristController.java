1f31236c7e930b31a4a31cb4c9eeb29e6e4e76ef
Kwanjai <65347403+Kwan1992@users.noreply.github.com>
2026-09-16 18:12:36 +0200

    delete unuse import

--- src/main/java/Controller/TouristController.java
+++ src/main/java/Controller/TouristController.java
@@ -1,7 +1,6 @@
 package Controller;
 
 import Model.TouristAttraction;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
